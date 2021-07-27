794db0387a671caf1736c43c0213736f5ada2e76
clebron949 <christopher.lebron1@gmail.com>
2021-07-27 08:00:16 -0400

    Added FizzBuzzModel Class

---
+++ Models/FizzBuzzModel.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FizzBuzz.Models
+{
+    public class FizzBuzzModel
+    {
+        public int Fizz { get; set; }
+        public int Buzz { get; set; }
+        public int Value { get; set; }
+
+        public FizzBuzzModel(string fizz, string buzz)
+        {
+            Fizz = Convert.ToInt32(fizz);
+            Buzz = Convert.ToInt32(buzz);
+        }
+
+        public string FizzBuzzCheck(string value)
+        {
+            Value = Convert.ToInt32(value);
+
+            if (Value % Fizz == 0)
+            {
+                value = "Fizz";
+            }
+
+            if (Value % Buzz == 0)
+            {
+                value = "Buzz";
+            }
+
+            if (Value % Fizz == 0 && Value % Buzz == 0)
+            {
+                value = "FizzBuzz";
+            }
+
+            return value;
+        }
+
+    }
+}
